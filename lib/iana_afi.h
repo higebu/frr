ef75fb29899ee42ebcf509066375a9739cdf0fb3
Yuya Kusakabe <y-kusakabe@bbsakura.net>
2023-01-19 16:03:28 +0900

    lib: add IANA_SAFI_MUP and SAFI_MUP
    A new SAFI value defined here to support BGP Extensions for the Mobile User Plane (MUP) SAFI.
    https://www.ietf.org/archive/id/draft-mpmz-bess-mup-safi-01.html
    
    Signed-off-by: Yuya Kusakabe <y-kusakabe@bbsakura.net>

--- lib/iana_afi.h
+++ lib/iana_afi.h
@@ -48,6 +48,7 @@ typedef enum {
 	IANA_SAFI_LABELED_UNICAST = 4,
 	IANA_SAFI_ENCAP = 7,
 	IANA_SAFI_EVPN = 70,
+	IANA_SAFI_MUP = 85,
 	IANA_SAFI_MPLS_VPN = 128,
 	IANA_SAFI_FLOWSPEC = 133
 } iana_safi_t;
@@ -102,6 +103,8 @@ static inline safi_t safi_iana2int(iana_safi_t safi)
 		return SAFI_LABELED_UNICAST;
 	case IANA_SAFI_FLOWSPEC:
 		return SAFI_FLOWSPEC;
+	case IANA_SAFI_MUP:
+		return SAFI_MUP;
 	default:
 		return SAFI_MAX;
 	}
@@ -124,6 +127,8 @@ static inline iana_safi_t safi_int2iana(safi_t safi)
 		return IANA_SAFI_LABELED_UNICAST;
 	case SAFI_FLOWSPEC:
 		return IANA_SAFI_FLOWSPEC;
+	case SAFI_MUP:
+		return IANA_SAFI_MUP;
 	default:
 		return IANA_SAFI_RESERVED;
 	}
