0bdb7a71f6af249fd6871204e6c4ae198debe204
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2026-05-04 22:26:21 +0900

    lib,bgpd: register SAFI_MUP for BGP-MUP
    Define SAFI_MUP=9 / IANA 85 per draft-ietf-bess-mup-safi.
    
    Add SAFI_MUP arms to existing safi_t switches as fall-throughs to
    keep -Wswitch-enum clean; real BGP-MUP behaviour follows in
    subsequent commits.
    
    Per draft section 3.3 the MP_REACH nexthop is always IPv6 regardless
    of NLRI AFI, so AFI=IPv4 SAFI=MUP routes encode an IPv6 nexthop.
    
    Signed-off-by: Yuya Kusakabe <yuya.kusakabe@gmail.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -107,6 +107,8 @@ enum bgp_af_index {
 	BGP_AF_IPV4_FLOWSPEC,
 	BGP_AF_IPV6_FLOWSPEC,
 	BGP_AF_BGP_LS,
+	BGP_AF_IPV4_MUP,
+	BGP_AF_IPV6_MUP,
 	BGP_AF_MAX
 };
 
@@ -3003,6 +3005,8 @@ static inline int afindex(afi_t afi, safi_t safi)
 			return BGP_AF_IPV4_ENCAP;
 		case SAFI_FLOWSPEC:
 			return BGP_AF_IPV4_FLOWSPEC;
+		case SAFI_MUP:
+			return BGP_AF_IPV4_MUP;
 		case SAFI_BGP_LS:
 		case SAFI_EVPN:
 		case SAFI_UNSPEC:
@@ -3024,6 +3028,8 @@ static inline int afindex(afi_t afi, safi_t safi)
 			return BGP_AF_IPV6_ENCAP;
 		case SAFI_FLOWSPEC:
 			return BGP_AF_IPV6_FLOWSPEC;
+		case SAFI_MUP:
+			return BGP_AF_IPV6_MUP;
 		case SAFI_BGP_LS:
 		case SAFI_EVPN:
 		case SAFI_UNSPEC:
@@ -3042,6 +3048,7 @@ static inline int afindex(afi_t afi, safi_t safi)
 		case SAFI_MPLS_VPN:
 		case SAFI_ENCAP:
 		case SAFI_FLOWSPEC:
+		case SAFI_MUP:
 		case SAFI_UNSPEC:
 		case SAFI_MAX:
 			return BGP_AF_MAX;
@@ -3058,6 +3065,7 @@ static inline int afindex(afi_t afi, safi_t safi)
 		case SAFI_ENCAP:
 		case SAFI_EVPN:
 		case SAFI_FLOWSPEC:
+		case SAFI_MUP:
 		case SAFI_UNSPEC:
 		case SAFI_MAX:
 			return BGP_AF_MAX;
